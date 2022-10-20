8c34e0fd62736ad416888d2e5b6d9df049272cae
siriyaoff <hwy16016@gmail.com>
2022-10-20 17:34:08 +0900

    feat(API): add API '/create'

---
+++ src/main/java/com/avatar2/home/web/InferenceController.java
@@ -0,0 +1,26 @@
+package com.avatar2.home.web;
+
+import com.avatar2.home.web.dto.InferenceRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+@RequiredArgsConstructor
+@RestController
+public class InferenceController {
+    @PostMapping("/create")
+    public InferenceRequestDto inference(@RequestBody InferenceRequestDto infDto){
+        WebClient webClient = WebClient.create("http://ec2-13-124-191-61.ap-northeast-2.compute.amazonaws.com:8080");
+        return webClient.post()
+                .uri("/inference")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(infDto)
+                .retrieve()
+                .bodyToMono(InferenceRequestDto.class)// response 파일
+                .block();
+    }
+}
