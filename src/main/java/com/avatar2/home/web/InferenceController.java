abba97918e0e26bf463306e99b35c102e7894104
siriyaoff <hwy16016@gmail.com>
2022-11-02 21:39:40 +0900

    feat(API): add API '/ptrig'(PostTrigger), '/webcl' for POST Multipartfile with WebClient

--- src/main/java/com/avatar2/home/web/InferenceController.java
+++ src/main/java/com/avatar2/home/web/InferenceController.java
@@ -14,15 +14,20 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.servlet.http.Cookie;
+import javax.sound.midi.Soundbank;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -33,19 +38,107 @@ import java.util.Arrays;
 
 @RestController
 public class InferenceController {
-    @PostMapping("/create")
-    public InferenceResponseDto inference(@RequestBody InferenceRequestDto infDto){
-        WebClient webClient = WebClient.create("http://ec2-13-124-191-61.ap-northeast-2.compute.amazonaws.com:8080");
-        return webClient.post()
-                .uri("/inference")
-                .contentType(MediaType.APPLICATION_JSON)
-                .bodyValue(infDto)
-                .retrieve()
-                .bodyToMono(InferenceResponseDto.class)// response 파일
-                .block();
+
+    @PostMapping("/infreq")
+    public InferenceResponseDto inference(MultipartHttpServletRequest req) {
+        try {
+            String height = req.getParameter("height");
+            MultipartFile psd = req.getFile("psd");
+            byte[] b = psd.getBytes();
+
+            InferenceRequestDto reqdto = InferenceRequestDto.builder()
+                    .height(Integer.parseInt(height))
+                    .psd(psd)
+                    .build();
+
+            WebClient webClient = WebClient.create("http://ec2-13-124-191-61.ap-northeast-2.compute.amazonaws.com:8080");
+            InferenceResponseDto resdto = webClient.post()
+                    .uri("/inference")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .bodyValue(reqdto)
+                    .retrieve()
+                    .bodyToMono(InferenceResponseDto.class)// response 파일
+                    .block();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+        }
+        return new InferenceResponseDto();
     }
 
-    @PostMapping("/reqdto")
+    @GetMapping("/ptrig") // trigger to post reqdto by webclient
+    public String ptrig() {
+        String res;
+        try {
+            File file = new File("/Users/yanghyowon/Downloads/serveriris.png");
+            FileItem fileItem = new DiskFileItem("originFile", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
+            InputStream input = new FileInputStream(file);
+            OutputStream os = fileItem.getOutputStream();
+            IOUtils.copy(input, os);
+            MultipartFile psd = new CommonsMultipartFile(fileItem);
+
+            InferenceRequestDto reqdto = InferenceRequestDto.builder()
+                    .height(165)
+                    .psd(psd)
+                    .build();
+
+            MultipartBodyBuilder builder = new MultipartBodyBuilder();
+            builder.part("height", reqdto.getHeight());
+            builder.part("curtime", reqdto.getCurtime());
+            builder.part("name", "psd");
+            builder.part("filename", "psd");
+            ByteArrayResource bar = new ByteArrayResource(reqdto.getPsd().getBytes()) {
+                @Override
+                public String getFilename() {
+                    return "psd";
+                }
+            };
+            builder.part("file", bar);
+
+//            builder.part("psd", reqdto.getPsd()); // multipartfile을 바로 넣으면 매핑 에러남
+
+
+            WebClient webClient = WebClient.create("http://localhost:8080");
+            res = webClient.post()
+                    .uri("/webcl")
+                    .body(BodyInserters.fromMultipartData(builder.build()))
+                    .exchange()
+                    .block()
+                    .bodyToMono(String.class)
+                    .block();
+        } catch (Exception e) {
+            System.out.println("In '/ptrig' :: " + e.getStackTrace()[0]);
+            res = "Error on '/ptrig' :: " + e.getMessage();
+        }
+        return res;
+    }
+    @PostMapping("/webcl") // receive Infreqdto as formdata by POST on Webclient
+    public String webcl(MultipartHttpServletRequest req) {
+        String res;
+        try {
+            int height = Integer.parseInt(req.getParameter("height"));
+            String curtime = req.getParameter("curtime");
+            MultipartFile psd = req.getFile("file");
+//            ByteArrayResource bar= (ByteArrayResource) req.getFile("file");
+//            if(bar==null) System.out.println("bar is empty**************************");
+//            MultipartFile psd = (MultipartFile) bar.getFile();
+
+//            MultipartFile psd = req.getFile("psd").getFile();
+//            byte[] psdb=req.getParameter("psd").getBytes();
+
+
+            FileUtils.writeByteArrayToFile(new File("/Users/yanghyowon/Downloads/webclpng.png"), psd.getBytes());
+
+            res = "curtime: " + curtime;
+        } catch (Exception e) {
+            System.out.println("In '/webcl' :: " + e.getStackTrace());
+//            System.out.println(e.getStackTrace());
+            res = "Error on '/webcl' :: " + e.getMessage();
+        }
+        return res;
+    }
+
+    @PostMapping("/reqdto") // receive Infreqdto by POST on <form>
     public String reqdto(MultipartHttpServletRequest request) throws IOException {
         try {
             String height = request.getParameter("height");
