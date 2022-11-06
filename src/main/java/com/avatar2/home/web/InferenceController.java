ce103d5b5866d7d5630867f8a0c6625fe4323505
siriyaoff <hwy16016@gmail.com>
2022-11-07 06:24:36 +0900

    feat(API): add /inftest

--- src/main/java/com/avatar2/home/web/InferenceController.java
+++ src/main/java/com/avatar2/home/web/InferenceController.java
@@ -3,9 +3,6 @@ package com.avatar2.home.web;
 import com.avatar2.home.domain.inference.PElem;
 import com.avatar2.home.web.dto.InferenceRequestDto;
 import com.avatar2.home.web.dto.InferenceResponseDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nimbusds.jose.JOSEObjectJSON;
-import net.bytebuddy.asm.Advice;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.disk.DiskFileItem;
 import org.apache.commons.io.FileUtils;
@@ -15,70 +12,35 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.servlet.http.Cookie;
-import javax.sound.midi.Soundbank;
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @RestController
 public class InferenceController {
-
-    @PostMapping("/infreq")
-    public InferenceResponseDto inference(MultipartHttpServletRequest req) {
+    /**
+     * for service
+     * @param req formdata
+     * @return resdto
+     */
+    @PostMapping("/create")
+    public InferenceResponseDto create(MultipartHttpServletRequest req) {
+        InferenceResponseDto resdto=null;
         try {
             String height = req.getParameter("height");
             MultipartFile psd = req.getFile("psd");
-            byte[] b = psd.getBytes();
-
             InferenceRequestDto reqdto = InferenceRequestDto.builder()
                     .height(Integer.parseInt(height))
-                    .psd(psd)
-                    .build();
-
-            WebClient webClient = WebClient.create("http://ec2-13-124-191-61.ap-northeast-2.compute.amazonaws.com:8080");
-            InferenceResponseDto resdto = webClient.post()
-                    .uri("/inference")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .bodyValue(reqdto)
-                    .retrieve()
-                    .bodyToMono(InferenceResponseDto.class)// response 파일
-                    .block();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println(e.getStackTrace());
-        }
-        return new InferenceResponseDto();
-    }
-
-    @GetMapping("/ptrig") // trigger to post reqdto by webclient
-    public String ptrig() {
-        String res;
-        try {
-            File file = new File("/Users/yanghyowon/Downloads/serveriris.png");
-            FileItem fileItem = new DiskFileItem("originFile", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
-            InputStream input = new FileInputStream(file);
-            OutputStream os = fileItem.getOutputStream();
-            IOUtils.copy(input, os);
-            MultipartFile psd = new CommonsMultipartFile(fileItem);
-
-            InferenceRequestDto reqdto = InferenceRequestDto.builder()
-                    .height(165)
                     .psd(psd)
                     .build();
 
@@ -95,66 +57,210 @@ public class InferenceController {
             };
             builder.part("file", bar);
 
+            WebClient webClient = WebClient.create("http://ec2-13-124-191-61.ap-northeast-2.compute.amazonaws.com:8080");
+            resdto = webClient.post()
+                    .uri("/inference")
+                    .body(BodyInserters.fromMultipartData(builder.build()))
+                    .retrieve()
+                    .bodyToMono(InferenceResponseDto.class)
+                    .block();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace()[0]);
+        }
+        return resdto;
+    }
+
+    /**
+     * request '/inference' in avt2-inference
+     * @return size of iris.png
+     */
+    @GetMapping("/inftest")
+    public String inftest() {
+        String res;
+        try {
+            // create InferenceRequestDto
+            File file = new File("/home/ec2-user/tpsd.psd");
+            FileItem fileItem = new DiskFileItem("originFile", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
+            InputStream input = new FileInputStream(file);
+            OutputStream os = fileItem.getOutputStream();
+            IOUtils.copy(input, os);
+            MultipartFile psd = new CommonsMultipartFile(fileItem);
+            InferenceRequestDto reqdto = InferenceRequestDto.builder()
+                    .height(165)
+                    .psd(psd)
+                    .build();
+
+            // create MultipartBodyBuilder
+            MultipartBodyBuilder builder = new MultipartBodyBuilder();
+            builder.part("height", reqdto.getHeight());
+            builder.part("curtime", reqdto.getCurtime());
+            builder.part("name", "psd");
+            builder.part("filename", "psd");
+            ByteArrayResource bar = new ByteArrayResource(reqdto.getPsd().getBytes()) {
+                @Override
+                public String getFilename() {
+                    return "psd";
+                }
+            };
+            builder.part("file", bar);
+
+            // create webclient, send request
+            ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                    .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(300 * 1024 * 1024))
+                    .build();
+            WebClient webClient = WebClient.builder()
+                    .exchangeStrategies(exchangeStrategies)
+                    .baseUrl("http://ec2-13-124-191-61.ap-northeast-2.compute.amazonaws.com:8080")
+                    .build();
+            InferenceResponseDto resdto = webClient.post()
+                    .uri("/inference")
+                    .body(BodyInserters.fromMultipartData(builder.build()))
+                    .retrieve()
+                    .bodyToMono(InferenceResponseDto.class)
+                    .block();
+            res = ""+resdto.getIris().getBytes().length;
+        } catch (Exception e) {
+            res = "Error on '/ptrig' :: " + e.getMessage();
+        }
+        return res;
+    }
+
+    /**
+     * request /webcl in local
+     * @return resdto.getErr()
+     */
+    @GetMapping("/ptrig")
+    public String ptrig() {
+        String res;
+        try {
+            // create InferenceRequestDto
+//            File file = new File("/Users/yanghyowon/Downloads/serveriris.png"); // mac
+            File file = new File("C:\\Users\\yang\\Downloads\\tpsd.psd"); // windows
+            FileItem fileItem = new DiskFileItem("originFile", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
+            InputStream input = new FileInputStream(file);
+            OutputStream os = fileItem.getOutputStream();
+            IOUtils.copy(input, os);
+            MultipartFile psd = new CommonsMultipartFile(fileItem);
+            InferenceRequestDto reqdto = InferenceRequestDto.builder()
+                    .height(165)
+                    .psd(psd)
+                    .build();
+
+            // create MultipartBodyBuilder
+            MultipartBodyBuilder builder = new MultipartBodyBuilder();
+            builder.part("height", reqdto.getHeight());
+            builder.part("curtime", reqdto.getCurtime());
+            builder.part("name", "psd");
+            builder.part("filename", "psd");
+            ByteArrayResource bar = new ByteArrayResource(reqdto.getPsd().getBytes()) {
+                @Override
+                public String getFilename() {
+                    return "psd";
+                }
+            };
+            builder.part("file", bar);
 //            builder.part("psd", reqdto.getPsd()); // multipartfile을 바로 넣으면 매핑 에러남
+            System.out.println("MultipartBodyBuilder created");
 
-
-            WebClient webClient = WebClient.create("http://localhost:8080");
-            res = webClient.post()
+            // create webclient, send request
+            ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                    .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(300 * 1024 * 1024))
+                    .build();
+            WebClient webClient = WebClient.builder()
+                    .exchangeStrategies(exchangeStrategies)
+                    .baseUrl("http://localhost:8080")
+                    .build();
+            InferenceResponseDto resdto = webClient.post()
                     .uri("/webcl")
                     .body(BodyInserters.fromMultipartData(builder.build()))
-                    .exchange()
-                    .block()
-                    .bodyToMono(String.class)
+                    .retrieve()
+                    .bodyToMono(InferenceResponseDto.class)
                     .block();
+            System.out.println("'/webcl' finished");
+            res = ""+resdto.getCoord().size()+" counts";
         } catch (Exception e) {
             System.out.println("In '/ptrig' :: " + e.getStackTrace()[0]);
             res = "Error on '/ptrig' :: " + e.getMessage();
         }
         return res;
     }
+
+    /**
+     * receive mhsr, return resdto
+     * @param req multipartbodybuilder
+     * @return resdto
+     */
     @PostMapping("/webcl") // receive Infreqdto as formdata by POST on Webclient
-    public String webcl(MultipartHttpServletRequest req) {
-        String res;
+    public InferenceResponseDto webcl(MultipartHttpServletRequest req) {
+        System.out.println("'/webcl' called");
+        InferenceResponseDto inferenceResponseDto;
         try {
             int height = Integer.parseInt(req.getParameter("height"));
             String curtime = req.getParameter("curtime");
             MultipartFile psd = req.getFile("file");
-//            ByteArrayResource bar= (ByteArrayResource) req.getFile("file");
-//            if(bar==null) System.out.println("bar is empty**************************");
-//            MultipartFile psd = (MultipartFile) bar.getFile();
 
-//            MultipartFile psd = req.getFile("psd").getFile();
-//            byte[] psdb=req.getParameter("psd").getBytes();
+            FileUtils.writeByteArrayToFile(new File("C:\\Users\\yang\\Downloads\\webclpng.png"),
+                    psd.getBytes());
 
+            // check reqdto successfully received
+            String res = "curtime: " + curtime;
+            System.out.println(res+"\nreqdto parsed");
 
-            FileUtils.writeByteArrayToFile(new File("/Users/yanghyowon/Downloads/webclpng.png"), psd.getBytes());
+            // create and return resdto
+            ArrayList<PElem> coord, eyelash;
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObject, jsonObject1;
+            JSONArray pointArray;
 
-            res = "curtime: " + curtime;
+            Reader reader = new FileReader("C:\\Users\\yang\\Downloads\\testPoint.json");
+            jsonObject = (JSONObject) parser.parse(reader);
+            pointArray = (JSONArray) parser.parse(jsonObject.get("Point").toString());
+            coord = new ArrayList<>();
+            for (Object obj : pointArray) {
+                jsonObject1 = (JSONObject) obj;
+                coord.add(PElem.builder()
+                        .id((String) jsonObject1.get("id"))
+                        .x((String) jsonObject1.get("x"))
+                        .y((String) jsonObject1.get("y"))
+                        .build());
+            }
+
+            reader = new FileReader("C:\\Users\\yang\\Downloads\\testPoint.json");
+            jsonObject = (JSONObject) parser.parse(reader);
+            pointArray = (JSONArray) parser.parse(jsonObject.get("Point").toString());
+            eyelash = new ArrayList<>();
+            for (Object obj : pointArray) {
+                jsonObject1 = (JSONObject) obj;
+                eyelash.add(PElem.builder()
+                        .id((String) jsonObject1.get("id"))
+                        .x((String) jsonObject1.get("x"))
+                        .y((String) jsonObject1.get("y"))
+                        .build());
+            }
+
+            File file = new File("C:\\Users\\yang\\Downloads\\serveriris.png");
+            FileItem fileItem = new DiskFileItem("originFile", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
+            InputStream input = new FileInputStream(file);
+            OutputStream os = fileItem.getOutputStream();
+            IOUtils.copy(input, os);
+            MultipartFile iris = new CommonsMultipartFile(fileItem);
+            inferenceResponseDto = new InferenceResponseDto(coord, eyelash, iris, "");
+            System.out.println(jsonObject);
+            System.out.println("\nresdto created");
         } catch (Exception e) {
-            System.out.println("In '/webcl' :: " + e.getStackTrace());
-//            System.out.println(e.getStackTrace());
-            res = "Error on '/webcl' :: " + e.getMessage();
+            String err = e.getStackTrace()[0].toString();
+            System.out.println("In '/webcl' :: " + err);
+            inferenceResponseDto = new InferenceResponseDto(null, null, null, err);
         }
-        return res;
+        return inferenceResponseDto;
     }
 
-    @PostMapping("/reqdto") // receive Infreqdto by POST on <form>
-    public String reqdto(MultipartHttpServletRequest request) throws IOException {
-        try {
-            String height = request.getParameter("height");
-            MultipartFile psd=request.getFile("psd");
-            byte[] b = psd.getBytes();
-
-            FileUtils.writeByteArrayToFile(new File("C:\\Users\\yang\\Downloads\\realtrue.png"), b);
-
-            return height+"\nheight printed";
-        } catch (Exception e) {
-            System.out.println(e.getStackTrace()[0]);
-        }
-        return "error";
-    }
-
-    @GetMapping("/corstest")
+    /**
+     * simple test with string for CORS test to EC2 instance 'avt2-inference'
+     * @return string received
+     */
+    @GetMapping("/corstest") // CORS resolved
     public String corstest() {
         WebClient webClient = WebClient.create("http://ec2-13-124-191-61.ap-northeast-2.compute.amazonaws.com:8080");
         return webClient.post()
@@ -164,24 +270,5 @@ public class InferenceController {
                 .retrieve()
                 .bodyToMono(String.class)// response 파일
                 .block();
-    }
-
-    @GetMapping("/mpfsave")
-    public String mpfsave() throws IOException, InterruptedException, FileNotFoundException, ParseException {
-
-        //File file = new File("/Users/yanghyowon/Downloads/tpsd.psd");
-        File file = new File("C:\\Users\\yang\\Downloads\\serveriris.png");
-        FileItem fileItem = new DiskFileItem("originFile", Files.probeContentType(file.toPath()), false, file.getName(), (int) file.length(), file.getParentFile());
-        InputStream input = new FileInputStream(file);
-        OutputStream os = fileItem.getOutputStream();
-        IOUtils.copy(input, os);
-        MultipartFile psd = new CommonsMultipartFile(fileItem);
-
-        byte[] b=psd.getBytes();
-        String psdb = Arrays.toString(b);
-
-        FileUtils.writeByteArrayToFile(new File("C:\\Users\\yang\\Downloads\\r3.png"), b);
-
-        return ""+psdb.length();
     }
 }
