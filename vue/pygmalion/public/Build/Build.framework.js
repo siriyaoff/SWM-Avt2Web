a1825c6cddd73940a8363c81c89c2091247b3620
siriyaoff <hwy16016@gmail.com>
2022-11-15 17:02:07 +0900

    feat(Unity): add Unity build files

---
+++ vue/pygmalion/public/Build/Build.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c6ebba46aa089531d78c3cb969a54c1cc32ccd6f1f9dffe9880ace9484d0a066
+size 391983
---
+++ vue/pygmalion/public/Build/Build.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:268a317fcde2cfd10b2e53e0914dbf9d7df66a8441b9657d21086fe940a2a75e
+size 18894
